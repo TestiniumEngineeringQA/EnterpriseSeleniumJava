fee4382e57de4b1815817561121184c505638ceb
mehmetaksahin <mehmet.aksahin@testinium.com>
2026-04-30 15:06:18 +0300

    Parameter Update

--- src/test/java/com/testinium/BaseTest.java
+++ src/test/java/com/testinium/BaseTest.java
@@ -56,10 +56,13 @@ public class BaseTest {
             // 2. ENVIRONMENT_PARAMETER
             String denemeParam = System.getenv("environmentParameter");
 
-            // 2. COMMAND_PARAMETER
-            Properties demoParam2 = System.getProperties();
+            // 1. COMMAND_PARAMETER
+            String demoParam2 = System.getProperty("commandParameter2");
             // 2. ENVIRONMENT_PARAMETER
-            Map <String, String> denemeParam2 = System.getenv();
+            String denemeParam2 = System.getenv("environmentParameter2");
+
+            System.out.println(">>> [COMMAND_PARAMETER] demo: " + demoParam);
+            System.out.println(">>> [ENVIRONMENT_PARAMETER] deneme: " + denemeParam);
 
             System.out.println(">>> [COMMAND_PARAMETER] demo2: " + demoParam2);
             System.out.println(">>> [ENVIRONMENT_PARAMETER] deneme2: " + denemeParam2);
